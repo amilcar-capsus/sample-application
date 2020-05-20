cda189ff555b7ea7bffd99c8abca99657b62760e
José Amílcar González Miranda <joseamilcargonzalezmiranda@iMac-de-Juan.local>
2020-05-20 11:20:45 -0500

    Initializing new bundles.

--- applications/geoportal/index.js
+++ applications/geoportal/index.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function() {
     Oskari.app.loadAppSetup(ajaxUrl + 'action_route=GetAppSetup', window.controlParams, function() {
         jQuery('#mapdiv').append('Unable to start');
+    }, function() {
+        Oskari.app.playBundle({ bundlename: 'layerlist' });
+        Oskari.app.playBundle({ bundlename: 'language-selector' });
     });
 });
